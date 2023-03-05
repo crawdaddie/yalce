be511adb663e631deba9d69043b578f59948b158
Adam Juraszek <adam@Adams-MacBook-Pro.local>
2023-03-05 13:17:17 +0000

    temp

--- test_file.sh
+++ test_file.sh
@@ -4,7 +4,7 @@ file=$1
 expects=$(grep -oE 'expect: .+$' $file | cut -c 9-)
 echo "$expects" > expected
 ./lang $file > actual 2>&1 # capture stderr to to file as well as stdout
-result=$(diff actual expected)
+result=$(diff --color=always actual expected)
 if [ -z "$result" ];
 then printf "\e[0;32m\xE2\x9C\x94\e[0m $file\n"
 else
@@ -12,4 +12,4 @@ printf "\e[0;31m\xE2\x9C\x98\e[0m  $file\n"
 fi
 echo "$result"
 
-rm actual expected
+# rm actual expected
