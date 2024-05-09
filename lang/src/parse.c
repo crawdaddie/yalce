da1513236788dda5333b9407d2c2fbec25f7dd52
crawdaddie <adam.a.juraszek@googlemail.com>
2024-05-09 10:16:24 +0100

    precedence parsing

--- lang/src/parse.c
+++ lang/src/parse.c
@@ -1,5 +1,6 @@
 #include "parse.h"
 #include "lex.h"
+#include "serde.h"
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -11,7 +12,8 @@ void init_parser(Parser *_parser, Lexer *lexer) {
   parser->lexer = lexer;
 }
 
-void advance() { parser->previous = parser->current;
+void advance() {
+  parser->previous = parser->current;
   parser->current = scan_token(parser->lexer);
 }
 
@@ -30,6 +32,7 @@ Ast *Ast_new(enum ast_tag tag) {
 }
 
 static Ast *parse_expression();
+
 static Ast *parse_let_statement() {
   advance();
   advance();
@@ -43,11 +46,10 @@ static Ast *parse_let_statement() {
   char *id = parser->previous.as.vstr;
   advance();
   ast_let->data.AST_LET.name = id;
-  ast_let->data.AST_LET.expr = parse_expression();
+  ast_let->data.AST_LET.expr = parse_expression(NULL);
   return ast_let;
 }
 
-static Ast *parse_expression();
 
 static Ast *parse_statement() {
   token tok = parser->current;
@@ -97,77 +99,83 @@ Ast *body_return(Ast *body) {
 // -------- parse expressions --------------
 ParserPrecedence token_to_precedence(token tok) {
   switch (tok.type) {
-    case TOKEN_ASSIGNMENT: return PREC_ASSIGNMENT;
-    case TOKEN_EQUALITY:
-    case TOKEN_NOT_EQUAL: return PREC_EQUALITY;
-    case TOKEN_LT:
-    case TOKEN_LTE:
-    case TOKEN_GT:
-    case TOKEN_GTE: return PREC_COMPARISON;
-    case TOKEN_PLUS:
-    case TOKEN_MINUS: return PREC_TERM;
-    case TOKEN_SLASH:
-    case TOKEN_STAR: 
-    case TOKEN_MODULO: return PREC_FACTOR;
-    case TOKEN_LEFT_SQ: return  PREC_INDEX;
-    case TOKEN_LP:
-    case TOKEN_RP: 
-    case TOKEN_DOT: return PREC_CALL;
-    case TOKEN_BANG: return PREC_UNARY;
-    case TOKEN_LOGICAL_OR: return PREC_OR;
-    case TOKEN_LOGICAL_AND: return PREC_AND;
-    default: return PREC_NONE;
+  case TOKEN_ASSIGNMENT:
+    return PREC_ASSIGNMENT;
+  case TOKEN_EQUALITY:
+  case TOKEN_NOT_EQUAL:   return PREC_EQUALITY;
+  case TOKEN_LT:
+  case TOKEN_LTE:
+  case TOKEN_GT:
+  case TOKEN_GTE:         return PREC_COMPARISON;
+  case TOKEN_PLUS:
+  case TOKEN_MINUS:       return PREC_TERM;
+  case TOKEN_SLASH:
+  case TOKEN_STAR:
+  case TOKEN_MODULO:      return PREC_FACTOR;
+  case TOKEN_LEFT_SQ:     return PREC_INDEX;
+  case TOKEN_LP:
+  case TOKEN_DOT:         return PREC_CALL;
+  case TOKEN_RP:          return PREC_NONE;
+  case TOKEN_BANG:        return PREC_UNARY;
+  case TOKEN_LOGICAL_OR:  return PREC_OR;
+  case TOKEN_LOGICAL_AND: return PREC_AND;
+  default:                return PREC_NONE;
   }
 }
 static Ast *parse_prefix();
-static Ast *parse_infix_expr() {
-  return NULL;
-}
+static Ast *parse_infix_expr() { return NULL; }
 
 static Ast *parse_precedence(Ast *prefix, ParserPrecedence precedence) {
   Ast *left = prefix;
+  // printf("precedence: %d\ncurrent token to prec: %d\n", precedence, token_to_precedence(parser->current));
+  // print_token(parser->current);
+  // printf("\nleft: ");
+  // print_ser_ast(left);
+  // printf("\n");
+  // printf("-----\n");
 
-  while (precedence < token_to_precedence(parser->current)) {
+
+  while (precedence <= token_to_precedence(parser->current)) {
     switch (parser->current.type) {
 
-      case TOKEN_PLUS:
-      case TOKEN_MINUS:
-      case TOKEN_SLASH:
-      case TOKEN_STAR:
-      case TOKEN_MODULO:
-      case TOKEN_EQUALITY:
-      case TOKEN_NOT_EQUAL:
-      case TOKEN_LT:
-      case TOKEN_LTE:
-      case TOKEN_GT:
-      case TOKEN_GTE: {
-        Ast *binop = Ast_new(AST_BINOP);
-        binop->data.AST_BINOP.op = parser->current.type; 
-        binop->data.AST_BINOP.left = left;
-        advance();
-        binop->data.AST_BINOP.right = parse_expression();
-        left = binop;
-        break;
-      }
-      default:
-        return left;
+    case TOKEN_PLUS:
+    case TOKEN_MINUS:
+    case TOKEN_SLASH:
+    case TOKEN_STAR:
+    case TOKEN_MODULO:
+    case TOKEN_EQUALITY:
+    case TOKEN_NOT_EQUAL:
+    case TOKEN_LT:
+    case TOKEN_LTE:
+    case TOKEN_GT:
+    case TOKEN_GTE: {
+      Ast *binop = Ast_new(AST_BINOP);
+      binop->data.AST_BINOP.op = parser->current.type;
+      binop->data.AST_BINOP.left = left;
+
+      advance();
+      binop->data.AST_BINOP.right = parse_expression();
+      left = binop;
+      break;
+    }
+    default:
+      return left;
     }
   }
   return left;
 };
+
 
 static Ast *parse_expression() {
   while (check(TOKEN_NL)) {
     advance();
   }
 
-
   // prefix
   Ast *prefix = parse_prefix();
-  Ast *ast = parse_precedence(prefix, PREC_NONE);
+  Ast *ast = parse_precedence(prefix, PREC_ASSIGNMENT);
   return ast;
 }
-
 
 static void push_arg(Ast *fn_application, Ast *arg) {
   if (arg) {
@@ -175,7 +183,8 @@ static void push_arg(Ast *fn_application, Ast *arg) {
     fn_application->data.AST_APPLICATION.len++;
     int len = fn_application->data.AST_APPLICATION.len;
 
-    fn_application->data.AST_APPLICATION.args = realloc(args, sizeof(Ast *) * len);
+    fn_application->data.AST_APPLICATION.args =
+        realloc(args, sizeof(Ast *) * len);
     fn_application->data.AST_APPLICATION.args[len - 1] = arg;
   }
 }
@@ -191,18 +200,18 @@ static void push_tuple_member(Ast *tuple, Ast *member) {
   }
 }
 
-#define AST_LITERAL_PREFIX(type, val) \
-    Ast *prefix = Ast_new(type); \
-    prefix->data.type.value = val
+#define AST_LITERAL_PREFIX(type, val)                                          \
+  Ast *prefix = Ast_new(type);                                                 \
+  prefix->data.type.value = val
 
 static bool is_terminator() {
   return check(TOKEN_RP) || check(TOKEN_DOUBLE_SEMICOLON) || check(TOKEN_NL);
 }
 static Ast *parse_grouping() {
   advance();
-  Ast *first = parse_expression();
+  Ast *first = parse_expression(NULL);
   if (check(TOKEN_COMMA)) {
-    Ast *tuple = Ast_new(AST_TUPLE); 
+    Ast *tuple = Ast_new(AST_TUPLE);
     tuple->data.AST_TUPLE.len = 1;
     tuple->data.AST_TUPLE.members = malloc(sizeof(Ast *));
     tuple->data.AST_TUPLE.members[0] = first;
@@ -211,12 +220,14 @@ static Ast *parse_grouping() {
     while (!check(TOKEN_RP)) {
       push_tuple_member(tuple, parse_expression());
       check(TOKEN_COMMA);
-
     }
     return tuple;
   }
-
-  return first;
+  if (parser->current.type == TOKEN_RP) {
+    advance();
+    return first;
+  }
+  return NULL;
 }
 
 static Ast *parse_prefix() {
@@ -259,7 +270,7 @@ static Ast *parse_prefix() {
     advance();
 
     while (!is_terminator()) {
-      Ast *arg = parse_expression();
+      Ast *arg = parse_expression(NULL);
       push_arg(prefix, arg);
     }
 
@@ -269,17 +280,16 @@ static Ast *parse_prefix() {
     Ast *prefix = Ast_new(AST_UNOP);
     prefix->data.AST_UNOP.op = parser->current.type;
     advance();
-    prefix->data.AST_UNOP.expr = parse_expression();
+    prefix->data.AST_UNOP.expr = parse_expression(NULL);
     return prefix;
   }
   case TOKEN_LP: {
-      return parse_grouping();
-    }
+    return parse_grouping();
+  }
   case TOKEN_IF:
   default: {
-      advance();
-      return NULL;
-    }
+    advance();
+    return NULL;
+  }
   }
 }
-
