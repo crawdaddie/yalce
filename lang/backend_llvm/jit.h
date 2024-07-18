00ad7fa959d2c22d9814c6dcab69b6c942f96139
Adam Juraszek <adam.juraszek@trydatabook.com>
2024-07-18 16:24:38 +0100

    fix codegen tests

--- lang/backend_llvm/jit.h
+++ lang/backend_llvm/jit.h
@@ -1,4 +1,7 @@
 #ifndef _LANG_BACKEND_LLVM_JIT_H
 #define _LANG_BACKEND_LLVM_JIT_H
+#include "llvm-c/ExecutionEngine.h"
 int jit(int argc, char **argv);
+
+int prepare_ex_engine(LLVMExecutionEngineRef *engine, LLVMModuleRef module);
 #endif
